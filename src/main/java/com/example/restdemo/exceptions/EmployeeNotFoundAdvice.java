eef72d9d4266401aca65ce8c32a39fdc96c27a97
mrdevsam <mdshoykotislam05@gmail.com>
2023-01-15 17:56:33 +0000

    fixing java file name

--- src/main/java/com/example/restdemo/exceptions/EmployeeNotFoundAdvice.java
+++ src/main/java/com/example/restdemo/exceptions/EmployeeNotFoundAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class EmployeeNitFoundAdvice {
+public class EmployeeNotFoundAdvice {
     
     @ResponseBody
     @ExceptionHandler(EmployeeNotFoundException.class)
