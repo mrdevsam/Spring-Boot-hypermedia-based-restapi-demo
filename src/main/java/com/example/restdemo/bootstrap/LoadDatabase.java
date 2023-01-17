7e2f96eee18e3e39f2c8c48c74ec46e941ec3782
mrdevsam <mdshoykotislam05@gmail.com>
2023-01-17 20:44:19 +0000

    updating bootstrap file

--- src/main/java/com/example/restdemo/bootstrap/LoadDatabase.java
+++ src/main/java/com/example/restdemo/bootstrap/LoadDatabase.java
@@ -7,17 +7,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.restdemo.model.Employee;
+import com.example.restdemo.model.OrdeR;
+import com.example.restdemo.model.Status;
 import com.example.restdemo.repositories.EmployeeRepository;
+import com.example.restdemo.repositories.OrderRepository;
 
 @Configuration
 public class LoadDatabase {
     private static final Logger logger = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository repository, OrderRepository orderRepository) {
         return args -> {
+
+            //employees
             logger.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
             logger.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+
+            repository.findAll().forEach(employee -> logger.info("Preloaded: " + employee));
+
+            //orders
+            orderRepository.save(new OrdeR("Mackbook Pro", Status.COMPLETED));
+            orderRepository.save(new OrdeR("Iphone 14 Pro", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> logger.info("Preloaded: " + order));
         };
     }
 }
