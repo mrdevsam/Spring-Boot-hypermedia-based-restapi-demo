d8f736e295fc226b22af67ea758860c7e1ea12ef
mrdevsam <mdshoykotislam05@gmail.com>
2023-01-17 11:38:45 +0000

    implementing order assembler and adding methods in the order controller

--- src/main/java/com/example/restdemo/bootstrap/OrderModelAssembler.java
+++ src/main/java/com/example/restdemo/bootstrap/OrderModelAssembler.java
@@ -1,8 +1,32 @@
 package com.example.restdemo.bootstrap;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.example.restdemo.controllers.OrderController;
+import com.example.restdemo.model.OrdeR;
+import com.example.restdemo.model.Status;
 
 @Component
-public class OrderModelAssembler {
-    
+public class OrderModelAssembler implements RepresentationModelAssembler<OrdeR, EntityModel<OrdeR>> {
+
+    @Override
+    public EntityModel<OrdeR> toModel(OrdeR entity) {
+
+        // unconditional links to single-item resource and aggregate-root
+        EntityModel<OrdeR> orderModel = EntityModel.of(entity,
+                linkTo(methodOn(OrderController.class).one(entity.getId())).withSelfRel(),
+                linkTo(methodOn(OrderController.class).all()).withRel("orders"));
+
+        // conditional links based on state of the order
+        if (entity.getStatus() == Status.IN_PROGRESS) {
+            orderModel.add(linkTo(methodOn(OrderController.class).cancel(entity.getId())).withRel("cancel"));
+
+            orderModel.add(linkTo(methodOn(OrderController.class).complete(entity.getId())).withRel("complete"));
+        }
+        return orderModel;
+    }
+
 }
--- src/main/java/com/example/restdemo/controllers/OrderController.java
+++ src/main/java/com/example/restdemo/controllers/OrderController.java
@@ -1,8 +1,41 @@
 package com.example.restdemo.controllers;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restdemo.bootstrap.OrderModelAssembler;
+import com.example.restdemo.model.OrdeR;
+import com.example.restdemo.repositories.OrderRepository;
 
 @RestController
 public class OrderController {
-    
+
+    private final OrderRepository orderRepository;
+    private final OrderModelAssembler orderModelAssembler;
+
+    public OrderController(OrderRepository orderRepository, OrderModelAssembler orderModelAssembler) {
+        this.orderRepository = orderRepository;
+        this.orderModelAssembler = orderModelAssembler;
+    }
+
+    @GetMapping("/orders")
+    public CollectionModel<EntityModel<OrdeR>> all() {
+        return null;
+    }
+
+    @GetMapping("/orders/{id}")
+    public EntityModel<OrdeR> one(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping("/orders")
+    public ResponseEntity<EntityModel<OrdeR>> newOrder(@RequestBody OrdeR order) {
+        return null;
+    }
 }
