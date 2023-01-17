52159c577e9f4728318ff87ebcfdad029a17cb04
mrdevsam <mdshoykotislam05@gmail.com>
2023-01-17 09:17:27 +0000

    creating new data model for ordering items

---
+++ src/main/java/com/example/restdemo/model/OrdeR.java
@@ -0,0 +1,82 @@
+package com.example.restdemo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER_ORDER")
+public class OrdeR {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private Status status;
+    
+    public OrdeR() {
+    }
+
+    public OrdeR(String description, Status status) {
+        this.description = description;
+        this.status = status;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrdeR other = (OrdeR) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (status != other.status)
+            return false;
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "OrdeR [id=" + id + ", description=" + description + ", status=" + status + "]";
+    }
+    
+}
---
+++ src/main/java/com/example/restdemo/model/Status.java
@@ -0,0 +1,8 @@
+package com.example.restdemo.model;
+
+public enum Status {
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
+
